7ec2b81219094840a8ba85b866ee9a1ee6a29860
DE MERINGO Olivier <olivier.demeringo@cimpa.com>
2024-07-02 13:01:56 +0200

    WIP: print full vm json

--- az-scan/src/main.rs
+++ az-scan/src/main.rs
@@ -2,6 +2,8 @@ use azure_identity::AzureCliCredential;
 use futures::stream::StreamExt;
 use std::sync::Arc;
 
+//use serde_json::Result;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let credential = Arc::new(AzureCliCredential::new());
@@ -14,10 +16,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let vms = vms?;
         count += vms.value.len();
         for vm in vms.value {
-            println!("{:?}", &vm.resource.id);
+            //println!("{:?}", &vm.resource.id);
+            let j = serde_json::to_string(&vm);
+           // println!("{:?}", &vm);
+           println!("{}", j.unwrap());
+            //println!("{:?}", j.unwrap());
         }
     }
-    println!("# of virtual machines {count}");
+    //println!("# of virtual machines {count}");
 
     Ok(())
 }
